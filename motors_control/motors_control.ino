ad8ab002a687fb193b5b444c81cda420ab2f8425
Zaha Mihai <Cataclismo@users.noreply.github.com>
2018-05-18 23:56:34 +0300

    Add command through serial and small refactoring

--- motors_control/motors_control.ino
+++ motors_control/motors_control.ino
@@ -1,43 +1,60 @@
 class PID
 {
-    private:
-        float m_Kp;
-        float m_Ki;
-        float m_Kd;
+public:
+    struct sData
+    {
+        sData()
+            : m_reference(0.f), m_lastError(0.f), m_sumError(0.f), m_diffError(0.f)
+        {
+        }
 
         float m_reference;
 
         float m_lastError;
         float m_sumError;
+        float m_diffError;
+    };
 
-    public:
-        PID()
-            : m_Kp(0.f), m_Ki(0.f), m_Kd(0.f), m_reference(0.f), m_lastError(0.f), m_sumError(0.f)
-        {
-        }
+private:
+    float m_Kp;
+    float m_Ki;
+    float m_Kd;
 
-        void setParameters(float Kp, float Ki, float Kd)
-        {
-            m_Kp = Kp;
-            m_Ki = Ki;
-            m_Kd = Kd;
-        }
+    sData m_data;
 
-        void setReference(float r)
-        {
-            m_reference = r;
-        }
+public:
+    PID()
+        : m_Kp(0.f), m_Ki(0.f), m_Kd(0.f), m_data()
+    {
+    }
 
-        float update(float measurement)
-        {
-            const float err = m_reference - measurement;
+    void setParameters(float Kp, float Ki, float Kd)
+    {
+        m_Kp = Kp;
+        m_Ki = Ki;
+        m_Kd = Kd;
+    }
 
-            const float diffError = m_lastError - err;
-            m_sumError += err;
-            m_lastError = err;
+    void setReference(float r)
+    {
+        m_data.m_reference = r;
+    }
 
-            return (m_Kp * err) + (m_Ki * m_sumError) + (m_Kd * diffError);
-        }
+    const char* data() const
+    {
+        return reinterpret_cast<const char *>(&m_data);
+    }
+
+    float update(float measurement)
+    {
+        const float err = m_data.m_reference - measurement;
+
+        m_data.m_diffError = m_data.m_lastError - err;
+        m_data.m_sumError += err;
+        m_data.m_lastError = err;
+
+        return (m_Kp * err) + (m_Ki * m_data.m_sumError) + (m_Kd * m_data.m_diffError);
+    }
 };
 
 struct sMotorPin
@@ -48,12 +65,26 @@ struct sMotorPin
     byte pwm;
 };
 
-typedef void (*func_type)(void);
+typedef void(*func_type)(void);
 
-const byte MOTORS = 2;
-const sMotorPin pins[MOTORS] = {
-    { 18, 7, 8, 10 },
-    { 19, 2, 4, 9 }
+enum eMotorSide
+{
+    RIGHT,
+    LEFT,
+    MOTOR_NUM
+};
+
+#pragma pack(push, 1)
+struct sCommand
+{
+    uint8_t fwd[MOTOR_NUM];
+    float distance[MOTOR_NUM];
+};
+#pragma pack(pop)
+
+const sMotorPin pins[MOTOR_NUM] = {
+  { 18, 7, 8, 10 }, // RIGHT
+  { 19, 2, 4, 9 }   // LEFT
 };
 
 const int32_t timerStart = 59286; // 65536-16MHz/256/2Hz (34286 for 0.5sec) (59286 for 0.1sec)
@@ -62,25 +93,29 @@ const int32_t ticksPerRevolution = 2240;
 const float distanceToRev = 1.f / (100.f * PI);
 const float Te = 0.1f;
 const float invTe = 10.f; // 1/Te
+const float speedLimit = 0.3f; // [m/s]
 
-int32_t encoder[MOTORS] = { 0, 0 };
-float distance[MOTORS] = { 0.f, 0.f };
-float speed[MOTORS] = { 0.f, 0.f };
-PID pidMotor[MOTORS];
+const byte feedbackPin = 0;
 
-void encoderInterrupt0()
+int32_t encoder[MOTOR_NUM] = { 0, 0 };
+float distance[MOTOR_NUM] = { 0.f, 0.f };
+float speed[MOTOR_NUM] = { 0.f, 0.f };
+PID pidMotor[MOTOR_NUM];
+PID pidDistance[MOTOR_NUM];
+
+void encoderInterruptRight()
 {
-    ++encoder[0];
+    ++encoder[RIGHT];
 }
 
-void encoderInterrupt1()
+void encoderInterruptLeft()
 {
-    ++encoder[1];
+    ++encoder[LEFT];
 }
 
-const func_type encoderInterrupts[MOTORS] = {
-    encoderInterrupt0,
-    encoderInterrupt1
+const func_type encoderInterrupts[MOTOR_NUM] = {
+    encoderInterruptRight,
+    encoderInterruptLeft
 };
 
 float minmax(float min, float value, float max)
@@ -90,81 +125,52 @@ float minmax(float min, float value, float max)
 
 ISR(TIMER1_OVF_vect)
 {
-    for (byte m = 0; m < MOTORS; ++m)
+    // reset timer
+    TCNT1 = timerStart;
+
+    for (byte m = 0; m < MOTOR_NUM; ++m)
     {
-        speed[m] = encoder[m] * encoder2Speed / ticksPerRevolution * invTe;
+        const float dist = encoder[m] * encoder2Speed / ticksPerRevolution;
+        speed[m] = dist * invTe; // V = d / T
+        distance[m] += dist;
+    }
+
+    for (byte m = 0; m < MOTOR_NUM; ++m)
+    {
+        float refSpeed = pidDistance[m].update(distance[m]);
+        refSpeed = minmax(0.f, refSpeed, speedLimit);
+
+        pidMotor[m].setReference(refSpeed);
+
+        const float pwm = pidMotor[m].update(speed[m]);
+        analogWrite(pins[m].pwm, minmax(0.f, pwm, 80.f));
     }
 
     memset(&encoder, 0, sizeof(encoder));
-
-    const float pwm[MOTORS] = {
-        pidMotor[0].update(speed[0]),
-        pidMotor[1].update(speed[1])
-    };
-
-    for (byte m = 0; m < MOTORS; ++m)
-    {
-        analogWrite(pins[m].pwm, minmax(0.f, pwm[m], 255.f));
-    }
-
-    for (byte m = 0; m < MOTORS; ++m)
-    {
-        distance[m] += speed[m] * Te;
-    }
-
-    // reset timer
-    TCNT1 = timerStart;
 }
 
-void startMotor(int id, bool fwd, bool bwd)
+void setDirection(byte side, bool fwd)
 {
-    digitalWrite(pins[id].fwd, fwd);
-    digitalWrite(pins[id].bwd, bwd);
+    digitalWrite(pins[side].fwd, fwd);
+    digitalWrite(pins[side].bwd, !fwd);
 }
 
-void stopMotor(int id)
+void stopMotor(byte side)
 {
-    digitalWrite(pins[id].fwd, LOW);
-    digitalWrite(pins[id].bwd, LOW);
-    analogWrite(pins[id].pwm, 0);
-}
-
-byte recData[(3 * sizeof(byte)) + sizeof(float)];
-void onReceive()
-{
-    if (Serial.available() >= sizeof(recData))
-    {
-        byte id = 0;
-        byte fwd = 0;
-        byte bwd = 0;
-        float speed = 0;
-        Serial.readBytes(&recData[0], sizeof(recData));
-        memcpy(&id, &recData[0], sizeof(id));
-        memcpy(&fwd, &recData[sizeof(id)], sizeof(fwd));
-        memcpy(&bwd, &recData[sizeof(id)+sizeof(fwd)], sizeof(bwd));
-        memcpy(&speed, &recData[sizeof(id)+sizeof(fwd)+sizeof(bwd)], sizeof(speed));
-        
-        // set direction
-        startMotor(id, fwd, bwd);
-        pidMotor[id].setReference(speed);
-    }
-}
-
-byte data[sizeof(float) * 2];
-void onRequest() // I2C
-{
-    memcpy(&data[0], &speed[0], sizeof(float));
-    memcpy(&data[sizeof(float)], &speed[1], sizeof(float));
-    Serial.write(data, sizeof(data));
+    digitalWrite(pins[side].fwd, LOW);
+    digitalWrite(pins[side].bwd, LOW);
+    analogWrite(pins[side].pwm, 0);
 }
 
 void setup()
 {
     Serial.begin(115200);
-    pidMotor[0].setParameters(50.f, 30.f, 0.f);
-    pidMotor[1].setParameters(50.f, 30.f, 0.f);
+    pidMotor[RIGHT].setParameters(80.f, 30.f, 0.f);
+    pidMotor[LEFT].setParameters(80.f, 30.f, 0.f);
+    pidDistance[RIGHT].setParameters(1.f, 0.f, 0.f);
+    pidDistance[LEFT].setParameters(1.f, 0.f, 0.f);
 
-    for (byte m = 0; m < MOTORS; ++m)
+    for (byte m = 0; m < MOTOR_NUM; ++m)
     {
         pinMode(pins[m].enc, INPUT_PULLUP);
         pinMode(pins[m].fwd, OUTPUT);
@@ -173,85 +179,61 @@ void setup()
         attachInterrupt(digitalPinToInterrupt(pins[m].enc), encoderInterrupts[m], CHANGE);
     }
 
+    pinMode(feedbackPin, OUTPUT);
+    digitalWrite(feedbackPin, LOW);
+
     noInterrupts();
 
     TCCR1A = 0;
     TCCR1B = 0;
     TCNT1 = timerStart;       // preload timer
-    TCCR1B |= (1 << CS12);    // 256 prescaler 
+    TCCR1B |= (1 << CS12);    // 256 prescaler
     TIMSK1 |= (1 << TOIE1);   // enable timer overflow interrupt
 
-    pinMode(21, INPUT);
+    pidDistance[RIGHT].setReference(0.f);
+    pidDistance[LEFT].setReference(0.f);
+
+    setDirection(RIGHT, true);
+    setDirection(LEFT, true);
 
     interrupts();
 }
 
-const float targetDistance = 0.5f; // [m]
-const float halfRotation = 12.5f * PI * 0.01f / 2.f; //[m]
-float crtTarget = 0.4f;
-byte state = 0;
+const float targetDistance = 1.f; // [m]
+const float halfRotation = 11.8f * PI * 0.01f; // [m]
+float crtTarget = 0.f;
 void loop()
 {
-    if (digitalRead(21) == 0)                                                                                                                                                                                                                                                                                                                                                                      
+    bool bDistanceReached = false;
+    for (byte m = 0; m < MOTOR_NUM; ++m)
     {
-        if (state == 0)
-        {
-            startMotor(0, true, false);
-            startMotor(1, true, false);
+        bDistanceReached |= (distance[m] >= crtTarget);
+    }
 
-            pidMotor[0].setReference(0.2f);
-            pidMotor[1].setReference(0.2f);
-            state = 1;
+    if (bDistanceReached)
+    {
+        memset(&distance, 0, sizeof(distance));
+        crtTarget = 0.f;
+        for (byte m = 0; m < MOTOR_NUM; ++m)
+        {
+            pidDistance[m].setReference(crtTarget);
         }
+
+        digitalWrite(feedbackPin, LOW);
+    }
+
+    if ((Serial.available() >= sizeof(sCommand)) && bDistanceReached)
+    {
+        digitalWrite(feedbackPin, HIGH);
         
-        bool bDistanceReached = false;
-        for (byte m = 0; m < MOTORS; ++m)
-        {
-            bDistanceReached |= distance[m] > crtTarget;
-        }
-    
-        if (bDistanceReached)
-        {
-            memset(&distance, 0, sizeof(distance));
-            startMotor(0, false, true);
-            startMotor(1, false, true);
-            delayMicroseconds(500);
-            stopMotor(0);
-            stopMotor(1);
+        sCommand command;
+        Serial.readBytes(reinterpret_cast<char *>(&command), sizeof(command));
 
-            ++state;
-
-            switch(state)
-            {
-                case 2:
-                {
-                    crtTarget = halfRotation;
-                    startMotor(0, false, true);
-                    startMotor(1, true, false);
-                }
-                break;
-                case 3:
-                {
-                    crtTarget = 0.55f;
-                    startMotor(0, true, false);
-                    startMotor(1, true, false);
-                }
-                break;
-                case 4:
-                {
-                    crtTarget = halfRotation;
-                    startMotor(0, false, true);
-                    startMotor(1, true, false);
-                }
-                break;
-                case 5:
-                {
-                    crtTarget = 0.5f;
-                    startMotor(0, true, false);
-                    startMotor(1, true, false);
-                }
-                break;
-            }
+        for (byte m = 0; m < MOTOR_NUM; ++m)
+        {
+            setDirection(m, command.fwd[m] == 1);
+            pidDistance[m].setReference(command.distance[m]);
+            crtTarget = command.distance[m];
         }
     }
 }
